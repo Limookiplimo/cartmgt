d83917a818977387ef3f824748cc9e7110735c85
Limoo Kiplimo <limoodrice@gmail.com>
2023-07-21 15:02:24 +0300

    Updated toml file to manage stock, prices, cart and sales terms.

--- cart.py
+++ cart.py
@@ -2,47 +2,40 @@ import pathlib
 import toml
 
 terms_path = pathlib.Path(__file__).parent / "terms.toml"
-terms = toml.loads(terms_path.read_text())
+cart_data = toml.loads(terms_path.read_text())
 
-def discounted_prices(item, quantity):
-    if item in terms:
-        item_terms = terms[item]["terms"]
+def discounted_prices(item, quantity, price):
+    if item in cart_data and "terms" in cart_data[item]:
+        item_terms = cart_data[item]["terms"]
         for term in item_terms:
-            if eval(f"{quantity} >= {term['size']}"):
-                return (1 - term['discount'] / 100) * quantity
-    return quantity
+            if eval(f"{quantity}{term['size']}"):
+                return price * (1 - term['discount'] / 100)
+    return price
 
-def get_items():
-    stocks = {"fat": 1000, "flour": 1000}
-    cart = {"fat": 15, "flour": 100}
-    price = {"fat": 1950, "flour": 2100}
-        
+def print_receipt():
     metadata = f"\n Hustler Wholesalers \n Customer Name: Your Name \n"
     print(metadata)
-    for item, stock_quantity in stocks.items():
-        cart_quantity = cart.get(item, 0)
-        cart_quantity = min(cart_quantity, stock_quantity)
-        total_price = cart_quantity * price[item]
 
+    remarks = f" \n You were served by: Limoo"
+
+    for item, item_data in cart_data.items():
+        if item == "meta":
+            continue
+
+        stock_quantity = item_data.get("stock", 0)
+        cart_quantity = item_data["cart"].get("quantity", 0)
+        cart_quantity = min(cart_quantity, stock_quantity)
+        item_price = item_data.get("price", 0)
+        orignal_amount = cart_quantity * item_price
 
         # Apply sales terms
-        discounted_quantity = discounted_prices(item, cart_quantity)
-        discounted_price = round(discounted_quantity * price[item])
-        discount = round(total_price - discounted_price)
+        discounted_price = discounted_prices(item, cart_quantity, item_price)
+        total_amount = discounted_price * cart_quantity
+        discount = orignal_amount - total_amount
 
-        remarks = f" \n You were served by: Limoo"
-        receipt = f"  {item}: \n  Quantity - {cart_quantity}, Discount - {discount}, Price - {discounted_price}"
+        receipt = f"  {item}: \n  Quantity - {cart_quantity}, UnitPrice - {item_price}, UnitPrice - {discount}, TotalPrice - {total_amount}"
+        print(receipt)
 
-        print(f"{receipt}")
     print(remarks)
 
-def apply_terms(path):
-    global terms
-    terms_info = toml.loads(path.read_text())
-    terms = { **terms, **terms_info}
-
-apply_terms(terms_path)
-get_items()
-
-
-
+print_receipt()
